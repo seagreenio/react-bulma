efabfb694b9b3034ef2d86f1eff612b9a45a8f2f
Yue Yang <g1enyy0ung@gmail.com>
2020-08-10 15:52:41 +0800

    feat: add Level

--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,4 +1,6 @@
 export { default as Container } from './Container'
 export type { ContainerProps } from './Container'
+export { default as Level } from './Level'
+export type { LevelProps } from './Level'
 export { default as Section } from './Section'
 export type { SectionProps } from './Section'
--- stories/elements/Columns.stories.tsx
+++ stories/elements/Columns.stories.tsx
@@ -1,14 +1,12 @@
-import { Column, Columns, withColor } from '../..'
+import { Section, Column, Columns, withColor } from '../..'
 
 import React from 'react'
-import centered from '@storybook/addon-centered/react'
 
 const ColorP = withColor('p')
 
 export default {
   title: 'Columns',
   component: Columns,
-  decorators: [centered],
 }
 
 const colorPProps = {
@@ -20,27 +18,29 @@ export const Default = () => {
   const style = { padding: '0.5rem 1rem' }
 
   return (
-    <Columns style={{ width: '60vw', textAlign: 'center' }}>
-      <Column>
-        <ColorP style={style} {...colorPProps}>
-          First column
-        </ColorP>
-      </Column>
-      <Column>
-        <ColorP style={style} {...colorPProps}>
-          Second column
-        </ColorP>
-      </Column>
-      <Column>
-        <ColorP style={style} {...colorPProps}>
-          Third column
-        </ColorP>
-      </Column>
-      <Column>
-        <ColorP style={style} {...colorPProps}>
-          Fourth column
-        </ColorP>
-      </Column>
-    </Columns>
+    <Section>
+      <Columns style={{ textAlign: 'center' }}>
+        <Column>
+          <ColorP style={style} {...colorPProps}>
+            First column
+          </ColorP>
+        </Column>
+        <Column>
+          <ColorP style={style} {...colorPProps}>
+            Second column
+          </ColorP>
+        </Column>
+        <Column>
+          <ColorP style={style} {...colorPProps}>
+            Third column
+          </ColorP>
+        </Column>
+        <Column>
+          <ColorP style={style} {...colorPProps}>
+            Fourth column
+          </ColorP>
+        </Column>
+      </Columns>
+    </Section>
   )
 }
