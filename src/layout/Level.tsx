1fe29a0c5dd177e9c0563a417b24fbac8c6ee634
Yue Yang <g1enyy0ung@gmail.com>
2020-08-10 15:52:41 +0800

    feat: add Level

---
+++ src/layout/Level.tsx
@@ -0,0 +1,47 @@
+import CommonProps from '../types/CommonProps'
+import React from 'react'
+import cx from 'classnames'
+import detectElementType from 'lib/detectElementType'
+import getUnhandledProps from 'lib/getUnhandledProps'
+
+export interface LevelProps extends CommonProps {
+  left?: boolean
+  right?: boolean
+  item?: boolean
+  mobile?: boolean
+}
+
+const Level: React.FC<LevelProps> = ({ children, className, ...props }) => {
+  const { left, right, item } = props
+  let levelClassName = 'level'
+  if (left) {
+    levelClassName = 'level-left'
+  } else if (right) {
+    levelClassName = 'level-right'
+  } else if (item) {
+    levelClassName = 'level-item'
+  }
+
+  const rest = getUnhandledProps(Level, props)
+  const ElementType = detectElementType(Level, props)
+  const classes = cx(levelClassName, className, {
+    [`is-mobile`]: props.mobile,
+  })
+
+  return (
+    <ElementType {...rest} className={classes}>
+      {children}
+    </ElementType>
+  )
+}
+
+Level.defaultProps = {
+  as: 'div',
+  className: undefined,
+  left: undefined,
+  right: undefined,
+  item: undefined,
+  mobile: undefined,
+}
+
+export default Level
---
+++ stories/layout/Level.stories.tsx
@@ -0,0 +1,41 @@
+import { Section, Level, LevelProps, withTypography } from '../..'
+
+import React from 'react'
+
+const TypographyLevel = withTypography<LevelProps>(Level)
+
+export default {
+  title: 'Level',
+  component: Level,
+}
+
+export const Default = () => (
+  <Section>
+    <Level as="nav">
+      <TypographyLevel item hasText="centered">
+        <div>
+          <p className="heading">Tweets</p>
+          <p className="title">3,456</p>
+        </div>
+      </TypographyLevel>
+      <TypographyLevel item hasText="centered">
+        <div>
+          <p className="heading">Following</p>
+          <p className="title">123</p>
+        </div>
+      </TypographyLevel>
+      <TypographyLevel item hasText="centered">
+        <div>
+          <p className="heading">Followers</p>
+          <p className="title">456K</p>
+        </div>
+      </TypographyLevel>
+      <TypographyLevel item hasText="centered">
+        <div>
+          <p className="heading">Likes</p>
+          <p className="title">789</p>
+        </div>
+      </TypographyLevel>
+    </Level>
+  </Section>
+)
