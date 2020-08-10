2e89392626792412fee154328d978c6d8d504345
Yue Yang <g1enyy0ung@gmail.com>
2020-08-10 16:49:48 +0800

    feat: add Footer

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,26 @@
+import CommonProps from 'types/CommonProps'
+import React from 'react'
+import cx from 'classnames'
+import detectElementType from 'lib/detectElementType'
+import getUnhandledProps from 'lib/getUnhandledProps'
+
+export type FooterProps = CommonProps
+
+const Footer: React.FC<FooterProps> = ({ children, className, ...props }) => {
+  const rest = getUnhandledProps(Footer, props)
+  const ElementType = detectElementType(Footer, props)
+  const classes = cx('footer', className)
+
+  return (
+    <ElementType {...rest} className={classes}>
+      {children}
+    </ElementType>
+  )
+}
+
+Footer.defaultProps = {
+  as: 'footer',
+  className: undefined,
+}
+
+export default Footer
