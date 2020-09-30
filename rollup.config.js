c19373f9e3081f3c1dbcf5b41bef3a11f8027c11
Yue Yang <g1enyy0ung@gmail.com>
2020-09-30 14:48:11 +0800

    chore: update

--- rollup.config.js
+++ rollup.config.js
@@ -21,16 +21,6 @@ const plugins = [
     extensions,
     exclude: 'node_modules/**',
     babelHelpers: 'runtime',
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: { node: 'current' },
-        },
-      ],
-      '@babel/preset-react',
-      '@babel/preset-typescript',
-    ],
     plugins: [
       [
         'module-resolver',
