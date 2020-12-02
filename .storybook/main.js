1b4d256d7950b7ed3723311da1b895d699612672
Yue Yang <g1enyy0ung@gmail.com>
2020-12-02 11:56:07 +0800

    chore(deps): upgrade

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
   addons: ['@storybook/addon-storysource'],
+  refs: {
+    rollup: { disable: true },
+    '@rollup/plugin-node-resolve': { disable: true },
+  },
 }
